c5c8edfdba93891d441686e2f94f0daa0b2fa530
Clément DURIN <clement.durin@epita.fr>
2021-05-19 11:48:53 +0200

    Partie 4 - une première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,10 +4,29 @@ public class Launcher {
     public static void main(String[] args){
         System.out.println("Bonjour a tous et bienvenue");
         Scanner scan = new Scanner(System.in);
-        String input = scan.next();
+        String input = scan.nextLine();
         while (! input.equals("quit")) {
-            System.out.println("Unknown command");
-            input = scan.next();
+            switch (input) {
+                case ("fibo") -> {
+                    System.out.println("Donner Rang");
+                    input = scan.nextLine();
+                    System.out.println(fibo(Integer.parseInt(input)));
+                }
+                default -> {
+                    System.out.println("Unknown command");
+                }
+            }
+            input = scan.nextLine();
         }
+    }
+
+    private static int fibo(int rang) {
+        if (rang < 0)
+            throw new IllegalArgumentException();
+        if (rang == 0)
+            return 0;
+        if (rang == 1)
+            return 1;
+        return fibo(rang - 1) + fibo(rang - 2);
     }
 }
